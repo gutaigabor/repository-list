d165bc1a81d587a2810c56af6ec15b505267ecb4
gaborgutai-pepperstone <gabor.gutai@pepperstone.com>
2024-12-13 12:02:59 +0100

    feat: Task 3 init

--- src/components/repo/repo.jsx
+++ src/components/repo/repo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import RepoStats from "../repo-list/ui/repo-stats/repo-stats";
+
 import styles from "./repo.module.css";
 
-const Repo = ({ onBack, repoData: { name, description } }) => (
+const Repo = ({ onBack, repoData: { name, description, forks_count, stargazers_count } }) => (
   <div className={styles.container}>
     <div className="box box--wide">
       <div className="title">{name}</div>
+      <RepoStats starCount={stargazers_count} forkCount={forks_count} />
       <div className={styles.content}>{description}</div>
     </div>
     <button onClick={onBack}>Back</button>
